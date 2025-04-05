938baa464faa51c1626f3d3bec9e69b5e6ea9984
JuanIgnGarcia <juan.ignacio.garcia.02@gmail.com>
2025-04-05 14:57:10 -0300

    user created and add_favorite

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -2,24 +2,30 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from src.service.database import get_db
 from src.service.user_service import UserService
-from src.request.user_request import UserAdminRequest, UserBuyerRequest
-from src.request.user_request import UserRequest
+from src.request.user_request import UserAdminRequest, UserBuyerRequest,UserRequest
+from src.request.favorite_request import FavoriteRequest
+from src.respond.favorite_response import FavoriteResponse
 
 router = APIRouter()
 
 service = UserService(get_db)
 
-@router.post("/admin", response_model=UserRequest)
-def create_admin(user_request: UserAdminRequest, db: Session = Depends(get_db)):
+@router.post("/admin", response_model=UserAdminRequest)
+def create_admin(username: str, password: str, db: Session = Depends(get_db)):
     service.set_session(db)
-    return service.create_user_admin(user_request)
+    return service.create_user_admin(username,password)
 
-@router.post("/buyer", response_model=UserRequest)
-def create_buyer(user_request: UserBuyerRequest, db: Session = Depends(get_db)):
+@router.post("/buyer", response_model=UserBuyerRequest)
+def create_buyer(username: str, password: str, db: Session = Depends(get_db)):
     service.set_session(db)
-    return service.create_user_buyer(user_request)
+    return service.create_user_buyer(username,password)
 
 @router.get("/", response_model=list[UserRequest])
 def get_all_users(db: Session = Depends(get_db)):
     service.set_session(db)
     return service.get_all_users()
+
+@router.post("/addFavorite/{user_id}", response_model=FavoriteResponse)
+def add_favorite(user_id:int,favorite_request: FavoriteRequest, db: Session = Depends(get_db)):
+    service.set_session(db)
+    return service.add_favorite(user_id,favorite_request)
--- src/request/favorite_request.py
+++ src/request/favorite_request.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 class FavoriteRequest(BaseModel):
     score: int = Field(..., ge=0, le=10)  # entre 0 y 10
     comment: str | None = None
-    product_id: str  
-    
+    product_id: str
+    product_title: str
+    product_price: float
+    product_currency: str
+    product_url: Optional[str]
+
     class Config:
         from_attributes = True
