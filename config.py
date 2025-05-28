aa803382567fb0ef4b91b33bdb95fbb05ddff965
JuanIgnGarcia <juan.ignacio.garcia.02@gmail.com>
2025-05-28 16:27:53 -0300

    secret and test

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# ML
+BASE_URL = "https://api.mercadolibre.com"
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+REFRESH_TOKEN = os.environ.get("REFRESH_TOKEN")
